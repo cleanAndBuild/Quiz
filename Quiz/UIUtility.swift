5368c143cf70840a5dc86636e42cedf3c07d5214
Clean & Build <32830149+cleanAndBuild@users.noreply.github.com>
2019-07-22 23:36:03 +0900

    Update UIUtility.swift

--- Quiz/UIUtility.swift
+++ Quiz/UIUtility.swift
@@ -12,30 +12,28 @@ import UIKit
 struct UIUtility {
 	//OKボタンのアラートを出す
 	static func showAlertWithOK(vc:UIViewController,title:String,message:String,handler: ((UIAlertAction) -> Swift.Void)? = nil,completion: (() -> Swift.Void)? = nil) {
-		let alert:UIAlertController = UIAlertController(title:title, message:message, preferredStyle: UIAlertControllerStyle.alert)
-		alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler:handler))
+        let alert:UIAlertController = UIAlertController(title:title, message:message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler:handler))
 		vc.present(alert, animated: true, completion:completion)
 	}
 	
 	//OKボタンとキャンセルのアラートを出す
 	static func showAlertWithOKAndCansel(vc:UIViewController,title:String,message:String,handlerOK: ((UIAlertAction) -> Swift.Void)? = nil,handlerCancel: ((UIAlertAction) -> Swift.Void)? = nil,completion: (() -> Swift.Void)? = nil) {
-		let alert:UIAlertController = UIAlertController(title:title, message:message, preferredStyle: UIAlertControllerStyle.alert)
-		alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler:handlerOK))
-		alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.default, handler:handlerCancel))
+        let alert:UIAlertController = UIAlertController(title:title, message:message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler:handlerOK))
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default, handler:handlerCancel))
 		
 		vc.present(alert, animated: true, completion:completion)
 	}
 	
 	//YESボタンとNOのアラートを出す
 	static func showAlertWithYESAndNO(vc:UIViewController,title:String,message:String,handlerYES: ((UIAlertAction) -> Swift.Void)? = nil,handlerNO: ((UIAlertAction) -> Swift.Void)? = nil,completion: (() -> Swift.Void)? = nil) {
-		let alert:UIAlertController = UIAlertController(title:title, message:message, preferredStyle: UIAlertControllerStyle.alert)
-		alert.addAction(UIAlertAction(title: "YES", style: UIAlertActionStyle.default, handler:handlerYES))
-		alert.addAction(UIAlertAction(title: "NO", style: UIAlertActionStyle.default, handler:handlerNO))
+        let alert:UIAlertController = UIAlertController(title:title, message:message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "YES", style: UIAlertAction.Style.default, handler:handlerYES))
+        alert.addAction(UIAlertAction(title: "NO", style: UIAlertAction.Style.default, handler:handlerNO))
 		
 		vc.present(alert, animated: true, completion:completion)
-		
 	}
-	
-	
 }
+
 
